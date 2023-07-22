9313b7b7a8319272e8dcc1899c2a13177c0e0e2a
SamKry <87971518+SamKry@users.noreply.github.com>
2023-07-22 12:32:08 +0200

    init core data model, semi working

--- TillTotal/TillTotalApp.swift
+++ TillTotal/TillTotalApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct TillTotalApp: App {
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
-    }
-}
+    }}
---
+++ TillTotal/Views/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  TillTotal
+//
+//  Created by Samuel Kreyenbühl on 22.07.23.
+//
+
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Coins.name, ascending: true)],
+        animation: .default)
+    private var coins: FetchedResults<Coins>
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(coins) { coin in
+                    NavigationLink {
+                        Text("Item at \(coin.name!)")
+                    } label: {
+                        Text(coin.color!)
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+            Text("Select an item")
+        }
+    }
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Coins(context: viewContext)
+            newItem.color = "red"
+            newItem.name = "the new Name"
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { coins[$0] }.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .long
+    formatter.timeStyle = .short
+    return formatter
+}()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
